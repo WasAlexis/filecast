937df58d06e48fcab550f0d0a235346135d234a8
Alexis <alexisnomain@icloud.com>
2025-12-18 16:49:12 -0400

    chore: add function to send by WebSocket

--- client/scripts/wsClient.js
+++ client/scripts/wsClient.js
@@ -5,13 +5,17 @@ import { loadName } from "./ui.js";
 const protocol = (window.location.protocol === 'https:') ? 'wss' : 'ws';
 const ws = new WebSocket(`${protocol}://${window.location.host}`);
 
+function sendSocket(data) {
+    ws.send(JSON.stringify(data));
+}
+
 ws.addEventListener('open', () => {
     const deviceName = (window.localStorage.getItem('deviceName')) ? window.localStorage.getItem('deviceName') : 'Uknown';
     if (deviceName != 'Uknown') {
         loadName(deviceName);
     }
-    ws.send(JSON.stringify({ signal: 'device-join', deviceName }));
+    sendSocket({ signal: 'device-join', deviceName });
     console.log('Connected to FileCast');
 });
 
-export default ws;
+export { ws, sendSocket};
